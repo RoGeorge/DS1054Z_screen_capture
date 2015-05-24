54a5c7203d8f19e03a6823d3e6bccdeb1817b803
RoGeorge <george.busuioc@gmail.com>
2015-05-25 01:33:37 +0300

    Separate code from captures

--- OscScreenGrabLAN.py
+++ OscScreenGrabLAN.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 __author__ = 'RoGeorge'
 #
+# TODO: Add timestamp and mark the trigger point as t0
+# TODO: Use channels label instead of chan1, chan2, chan3, chan4, math
 # TODO: Add command line parameters file path
 # TODO: Add GUI
 # TODO: Add browse and custom filename selection
@@ -33,7 +35,7 @@ company = 0
 model = 1
 serial = 2
 
-# Check parameters
+# Check command line parameters
 script_name = os.path.basename(sys.argv[0])
 
 
@@ -41,14 +43,14 @@ def print_help():
     # Print usage
     print
     print "Usage:"
-    print "    " + script_name + " png|bmp|csv [oscilloscope_IP [save_path]]"
+    print "    " + "python " + script_name + " png|bmp|csv [oscilloscope_IP [save_path]]"
     print
     print "Usage examples:"
-    print "    " + script_name + " png"
+    print "    " + "python " + script_name + " png"
+    print "    " + "python " + script_name + " csv 192.168.1.3"
     print
     print "The following usage cases are not yet implemented:"
-    print "    " + script_name + " csv 192.168.1.3"
-    print "    " + script_name + " bmp 192.168.1.3 my_place_for_osc_bmp_captures"
+    print "    " + "python " + script_name + " bmp 192.168.1.3 my_place_for_captures"
     print
     print "This program captures either the waveform or the whole screen"
     print "    of a Rigol DS1000Z series oscilloscope, then save it on the computer"
@@ -61,7 +63,7 @@ def print_help():
     print "    No VISA, IVI or Rigol drivers are needed."
     print
 
-
+# Read/verify file type
 if len(sys.argv) <= 1:
     print_help()
     sys.exit("Warning - wrong command line parameters.")
@@ -71,6 +73,10 @@ elif sys.argv[1].lower() not in ["png", "bmp", "csv"]:
     sys.exit("ERROR")
 
 file_format = sys.argv[1].lower()
+
+# Read IP
+if len(sys.argv) > 1:
+    IP_DS1104Z = sys.argv[2]
 
 # Create/check if 'path' exists
 
@@ -108,7 +114,7 @@ if (id_fields[company] != "RIGOL TECHNOLOGIES") or \
     print "ERROR: No Rigol from series DS1000Z found at ", IP_DS1104Z
     sys.exit("ERROR")
 
-print "Instrument ID:"
+print "Instrument ID:",
 print instrument_id
 
 # Prepare filename as C:\MODEL_SERIAL_YYYY-MM-DD_HH.MM.SS
@@ -118,7 +124,7 @@ filename = path_to_save + id_fields[model] + "_" + id_fields[serial] + "_" + tim
 if file_format in ["png", "bmp"]:
     # Ask for an oscilloscope display print screen
     tn.write("display:data?")
-    print "Receiving..."
+    print "Receiving screen capture..."
     buff = tn.read_until("\n", big_wait)
 
     # Just in case the transfer did not complete in the expected time
@@ -137,7 +143,6 @@ if file_format in ["png", "bmp"]:
     print "Saved file:", filename + "." + file_format
 
 elif file_format == "csv":
-    print "csv mode"
     # Put osc in STOP mode
     # tn.write("stop")
     # response = tn.read_until("\n", 1)
@@ -147,12 +152,13 @@ elif file_format == "csv":
     for channel in ["chan1", "chan2", "chan3", "chan4", "math"]:
         tn.write(channel + ":display?")
         response = tn.read_until("\n", 1)
+
         # Strip '\n' terminator
         response = response[:-1]
         if response == '1':
             channel_list += [channel]
 
-    print channel_list
+    print "Active channels on the display:", channel_list
 
     csv_buff = ""
 
@@ -162,19 +168,19 @@ elif file_format == "csv":
 
         # Set WAVE parameters
         tn.write("waveform:source " + channel)
-        response = tn.read_until("\n", 1)
+        time.sleep(0.2)
 
         # Maximum - only displayed data when osc. in RUN mode, or full memory data when STOPed
-        tn.write("waveform:mode maximum")
-        response = tn.read_until("\n", 1)
+        tn.write("waveform:mode normal")
+        time.sleep(0.2)
 
         tn.write("waveform:format ASCII")
-        response = tn.read_until("\n", 1)
+        time.sleep(0.2)
 
         # Get data
         tn.write("waveform:data?")
 
-        print "Receiving..."
+        print "Receiving data for channel " + str(channel) + "..."
         buff = tn.read_until("\n", big_wait)
 
         # Just in case the transfer did not complete in the expected time
@@ -193,14 +199,9 @@ elif file_format == "csv":
         buff = buff[TMC_header_len:-1]
 
         # Process data
-        print buff
-        print
-        print "data_points =", data_points, "from", channel
 
         buff_list = buff.split(",")
         buff_rows = len(buff_list)
-
-        print buff_list
 
         # Put red data into csv_buff
         csv_buff_list = csv_buff.split(os.linesep)
@@ -223,8 +224,6 @@ elif file_format == "csv":
                     csv_buff += str(csv_buff_list[current_row]) + "," + str(point) + os.linesep
                 else:
                     csv_buff += "," + str(point) + os.linesep
-
-        print csv_buff
 
     # Save data as CSV
     scr_file = open(filename + "." + file_format, "wb")
--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # DS1054Z_screen_capture
 'OscScreenGrabLAN.py' is a Python script that captures
 whatever is displayed on the screen of a Rigol DS1000Z series oscilloscope.
+
+It can save data as a WYSIWYG (What You See Is What You Get) picture of the oscilloscope screen,
+ or as a text file in CSV (Comma Separated Values) format.
+
 To achieve this, SCPI (Standard Commands for Programmable Instruments) are sent from the computer
 to the oscilloscope, using the LXI (LAN-based eXtensions for Instrumentation) protocol over a Telnet connection.
 The computer and the oscilloscope are connected together by a LAN (Local Area Network).
@@ -11,6 +15,33 @@ Python 2 is required. Python 3 is not supported.
 Tested with Windows 7, Python 2.7.9 and Rigol DS1104Z (a fully upgraded DS1054Z oscilloscope).
 
 Tested in a VMware machine with Linux Debian 7.7.0, Python 2.7.3 and PIL.
+
+
+User Manual:
+-----------
+Usage:
+
+    python OscScreenGrabLAN.py png|bmp|csv [oscilloscope_IP [save_path]]
+
+Usage examples:
+
+    python OscScreenGrabLAN.py png
+    python OscScreenGrabLAN.py csv 192.168.1.3
+
+The following usage case is not yet implemented:
+
+    python OscScreenGrabLAN.py bmp 192.168.1.3 my_place_for_captures
+
+This program captures either the waveform or the whole screen
+    of a Rigol DS1000Z series oscilloscope, then save it on the computer
+    as a CSV, PNG or BMP file with a timestamp in the file name.
+
+    The program is using LXI protocol, so the computer
+    must have LAN connection with the oscilloscope.
+    USB and/or GPIB connections are not used by this software.
+
+    No VISA, IVI or Rigol drivers are needed.
+
 
 Windows Usage:
 -------------
@@ -23,7 +54,8 @@ open a Command Prompt as Administrator and run "OscScreenGrabLAN.py".
 
 Example:
 
-    C:\Python27\python.exe OscScreenGrabLAN.py
+    C:\Python27\python.exe OscScreenGrabLAN.py png 192.168.1.3
+    C:\Python27\python.exe OscScreenGrabLAN.py csv 192.168.1.3
     
 Linux Usage:
 -----------
@@ -34,5 +66,6 @@ go to the project's directory and run "OscScreenGrabLAN.py".
 
 Example:
 
-    python OscScreenGrabLAN.py
+    python OscScreenGrabLAN.py png 192.168.1.3
+    python OscScreenGrabLAN.py csv 192.168.1.3
     
